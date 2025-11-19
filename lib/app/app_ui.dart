d005ec2904abe009cc111eeee99ecdc1147e50ed
Nemer7 <nemervelandia@gmail.com>
2025-11-19 14:47:23 -0500

    feat: Update initial route handling for web and mobile in AppUi and add .htaccess for Apache redirection

--- lib/app/app_ui.dart
+++ lib/app/app_ui.dart
@@ -1,5 +1,7 @@
+import 'package:ema_educacion_medica_avanzada/admin/config/admin_routes.dart';
 import 'package:ema_educacion_medica_avanzada/config/config.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:get/get.dart';
 
 class AppUi extends StatelessWidget {
@@ -7,10 +9,14 @@ class AppUi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // En web, redirigir directamente al panel admin
+    // En móvil, usar la pantalla de inicio normal
+    final String initialRoute = kIsWeb ? AdminRoutes.login : Routes.start.name;
+
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       getPages: AppPages.routes,
-      initialRoute: Routes.start.name,
+      initialRoute: initialRoute,
       theme: AppTheme.getTheme(),
       title: 'ema Educacion Medica Avanzada',
     );
